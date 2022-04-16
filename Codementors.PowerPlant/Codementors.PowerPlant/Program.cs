98f618f8ceaf609d0e32073fe7b5605bccd2c0f2
Dominik <ddunst7@gmail.com>
2022-04-16 19:10:01 +0200

    [HW4] Initial commit, add PowerPlant library

---
+++ Codementors.PowerPlant/Codementors.PowerPlant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codementors.PowerPlant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
