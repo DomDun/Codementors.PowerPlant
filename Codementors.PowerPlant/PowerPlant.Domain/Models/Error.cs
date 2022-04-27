2d812c20b9d33460a11bbe9188cd0ed6fd7282c3
Dominik <ddunst7@gmail.com>
2022-04-27 16:16:27 +0200

    [HW4] repair project

---
+++ Codementors.PowerPlant/PowerPlant.Domain/Models/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PowerPlantCzarnobyl.Domain.Models
+{
+    public class Error
+    {
+        public string PlantName { get; set; }
+        public string MachineName { get; set; }
+        public double MachineValue { get; set; }
+        public string Unit { get; set; }
+        public DateTime errorTime { get; set; }
+    }
+}
---
+++ Codementors.PowerPlant/PowerPlant.Domain/Models/ProducedPower.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PowerPlantCzarnobyl.Domain.Models
+{
+    public class ProducedPower
+    {
+
+    }
+}
